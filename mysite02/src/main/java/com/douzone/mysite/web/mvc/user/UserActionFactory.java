1c94a27db9985127b4585ed189f50466f043284d
JSB1012 <sbj15102@naver.com>
2022-05-25 12:16:01 +0900

    main controller #final

---
+++ mysite02/src/main/java/com/douzone/mysite/web/mvc/user/UserActionFactory.java
@@ -0,0 +1,23 @@
+package com.douzone.mysite.web.mvc.user;
+
+import com.douzone.mysite.web.mvc.main.DefaultAction;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
+
+public class UserActionFactory extends ActionFactory {
+	Action action = null;
+
+	@Override
+	public Action getAction(String actionName) {
+		if("joins".equals(actionName)) {
+			//action = new JoinAction();
+			
+		}else if("".equals(actionName)) {
+			
+		} else {
+			action = new DefaultAction();
+		}
+		return action;
+	}
+
+}
