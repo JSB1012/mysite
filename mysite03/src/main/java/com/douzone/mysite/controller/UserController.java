aa6e14d7dedc75ca8483142efd40cccdc2fd5ba1
JSB1012 <sbj15102@naver.com>
2022-06-07 15:21:40 +0900

    mysite - user#2

---
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -0,0 +1,89 @@
+package com.douzone.mysite.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.douzone.mysite.service.UserService;
+import com.douzone.mysite.vo.UserVo;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET) //GET방식으로 들어올때 
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST) //POST방식으로 들어옴 같은 join이라도 GET,POST 나눠서 같은 이름 사용 
+	public String join(UserVo vo) {
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping("/joinsuccess") 
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET) 
+	public String login() {
+		return "user/login";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)  
+	public String login(HttpSession session, UserVo vo, Model model) {
+		UserVo authUser = userService.getUser(vo);
+		if(authUser == null) {
+			model.addAttribute("result", "fail");
+			model.addAttribute("email", vo.getEmail());
+			return "user/login";
+		}
+		
+		/* 인증 처리 */
+		session.setAttribute("authUser", authUser);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("authUser");
+		session.invalidate();
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.GET) //GET방식으로 들어올때 
+	public String update(HttpSession session, Model model) {
+		// 접근 제어(Access Control)//////////////////////////////////
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		////////////////////////////////////////////////////////////
+		Long no = authUser.getNo();
+		UserVo userVo =userService.getUser(no);
+		model.addAttribute("userVo", userVo);
+		return "user/update";
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.POST) 
+	public String update(HttpSession session, UserVo vo) {
+		// 접근 제어(Access Control)//////////////////////////////////
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		////////////////////////////////////////////////////////////
+		System.out.println(vo);
+		return "redirect:/user/update";
+	}
+
+}
