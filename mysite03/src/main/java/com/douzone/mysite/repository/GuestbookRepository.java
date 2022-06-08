78da1703c27749e71ed4145406b4c7d2f399574c
JSB1012 <sbj15102@naver.com>
2022-06-08 17:28:05 +0900

    mybatis #finish

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -1,143 +1,37 @@
 package com.douzone.mysite.repository;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import javax.sql.DataSource;
-
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.douzone.mysite.exception.GuestbookRepositoryException;
 import com.douzone.mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
+	
 	@Autowired
-	private DataSource dataSource;
-
-	public boolean insert(GuestbookVo vo) {
-		boolean result = false;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			connection = dataSource.getConnection();
-
-			String sql = "insert into guestbook " + " values(null, ?, ?, ?, now()) ";
-			pstmt = connection.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getMessage());
-
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-		} catch (SQLException e) {
-			// System.out.println("드라이버 로딩 실패:" + e);
-			throw new GuestbookRepositoryException(e.toString());
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
-	}
-
-	public boolean delete(GuestbookVo vo) {
-		boolean result = false;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			connection = dataSource.getConnection();
-
-			String sql = "delete from guestbook" + " where no = ?" + " and password = ?";
-			pstmt = connection.prepareStatement(sql);
-
-			pstmt.setLong(1, vo.getNo());
-			pstmt.setString(2, vo.getPassword());
-
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
-	}
+	private SqlSession sqlSession;
 
 	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> result = new ArrayList<>();
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		return sqlSession.selectList("guestbook.findAll");
+	}
+	
+	public boolean delete(Long no, String password) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("no", no);
+		map.put("password", password);
+		return sqlSession.delete("guestbook.delete", map) ==1;
+	}
 
-		try {
-			connection = dataSource.getConnection();
-
-			String sql = "select no, name,date_format(reg_date, '%Y-%m-%d %H:%i:%s'), message" + " from guestbook"
-					+ " order by reg_date desc";
-
-			pstmt = connection.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				String reg_Date = rs.getString(3);
-				String message = rs.getString(4);
-
-				GuestbookVo vo = new GuestbookVo();
-				vo.setNo(no);
-				vo.setName(name);
-				vo.setRegDate(reg_Date);
-				vo.setMessage(message);
-
-				result.add(vo);
-			}
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
+	public boolean insert(GuestbookVo vo) {
+		System.out.println(vo);
+		boolean result = sqlSession.insert("guestbook.insert", vo) ==1;
+		System.out.println(vo);
 		return result;
 	}
+
+
+
 }
--- mysite03/src/main/java/com/douzone/mysite/service/GuestbookService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/GuestbookService.java
@@ -23,11 +23,7 @@ public class GuestbookService {
 	}
 
 	public Boolean deleteMessage(Long no, String password) {
-		GuestbookVo vo = new GuestbookVo();
-		vo.setNo(no);
-		vo.setPassword(password);
-
-		return guestbookRepository.delete(vo);
+		return guestbookRepository.delete(no,password);
 	}
 
 }
