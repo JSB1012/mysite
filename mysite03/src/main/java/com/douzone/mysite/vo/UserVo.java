2683c0d0dcaa44c14dea177dabf6165bdeda165a
JSB1012 <sbj15102@naver.com>
2022-06-13 17:31:00 +0900

    어드민 페이지 #1

--- mysite03/src/main/java/com/douzone/mysite/vo/UserVo.java
+++ mysite03/src/main/java/com/douzone/mysite/vo/UserVo.java
@@ -6,6 +6,7 @@ public class UserVo {
 	private String email;
 	private String password;
 	private String gender;
+	private String role;
 	private String joinDate;
 
 	public Long getNo() {
@@ -47,6 +48,14 @@ public class UserVo {
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 	public String getJoinDate() {
 		return joinDate;
@@ -59,7 +68,7 @@ public class UserVo {
 	@Override
 	public String toString() {
 		return "UserVo [no=" + no + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
-				+ gender + ", joinDate=" + joinDate + "]";
+				+ gender + ", role=" + role + ", joinDate=" + joinDate + "]";
 	}
 
 }
